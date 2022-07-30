a8e5fdc64c0e309c74c126095aa857b35831250c
Yousef <yousefsaleh20@icloud.com>
2022-07-30 15:21:54 -0500

    commit tree aa3947654402ad1b102676441b144e545f2c5027

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function writeCards(namesArray, eventName){
+    let messages = []
+    
+    for (let i = 0; i < namesArray.length ; i++) {
+        messages.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown() {
+    let countDown = 10;
+while (countDown >= 0) {
+  console.log(countDown--);
+}
+}
